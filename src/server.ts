7e262059c96f083ad38a33cdd17dfbc35220677f
LucasBFerraz <lucastadeuferraz@gmail.com>
2025-09-11 06:09:23 -0300

    feat: Implement Docker Integration for Project Management

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,7 @@ server.register(import('./routes/projects'), { prefix: '/projects' });
 server.register(import('./routes/filetree'), { prefix: '/projects/:projectId/filetree' });
 server.register(import('./routes/files'), { prefix: '/projects/:projectId/files' });
 server.register(import('./routes/bash'), { prefix: '/projects/:projectId/bash' });
+server.register(import('./routes/docker'), { prefix: '/projects/:projectId/docker' });
 
 
 // Serve openapi.json at /openapi
